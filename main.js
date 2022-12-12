d01e682bb5d0806958b93697ccaa6a3be51466eb
Akshay Khona <DesertSled800c@gmail.com>
2022-12-12 10:28:16 -0600

    proj complete

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (dot of clay) {
+   const mug = `${dot} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln);
